f462141a6e44b7272b6ab84c069f71344cee8f21
Will Passidomo <wpassidomo@mparticle.com>
2022-05-05 15:08:37 -0400

    upgrade: update AGP, Gradle, MParticle Android SDK
    - update android-core to 5.38.2
    - update AGP to 7.1.2
    - updage Gradle to 7.3.2
    - update targetSdk, compileSdk to 31
    - fix compilation errors
    - migrade build files to kts
    - and Android unit test CI workflow

---
+++ plugin/settings.gradle.kts
@@ -0,0 +1,10 @@
+include(":src:android")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.library") version ("7.1.3")
+    }
+}
